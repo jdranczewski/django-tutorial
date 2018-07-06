584d40e85ca2e03c5a8321b72bafb52b9808d48b
Jakub Dranczewski <domdlapsa@gmail.com>
2018-07-06 13:26:41 +0200

    Raising 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -10,6 +11,10 @@ def index(request):
 
 
 def detail(request, question_id):
+    try:
+        q = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
     return HttpResponse("You're looking at question %s." % question_id)
 
 
