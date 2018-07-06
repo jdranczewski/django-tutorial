4c29cfece5c73b6b1f4731b033630f4e3b11fa4d
Jakub Dranczewski <domdlapsa@gmail.com>
2018-07-06 13:29:17 +0200

    'gt_object_or_404' shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
@@ -11,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        q = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    q = get_object_or_404(Question, pk=question_id)
     return HttpResponse("You're looking at question %s." % question_id)
 
 
