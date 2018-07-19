2d6e3232f506eaf48793eff8abaf45135e8d5249
Jakub Dranczewski <domdlapsa@gmail.com>
2018-07-19 17:58:49 +0200

    More test Better thorough than sorry

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,9 +5,20 @@ from django.utils import timezone
 
 from .models import Question
 
+
 # Create your tests here.
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() + datetime.timedelta(minutes=-10)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() + datetime.timedelta(days=-30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
